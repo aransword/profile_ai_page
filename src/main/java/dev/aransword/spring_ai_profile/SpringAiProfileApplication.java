51f46a6354a32107f9026e3c527c2f2376ac3227
aransword <lupin521@gmail.com>
2026-04-02 14:40:27 +0900

    Initial commit

---
+++ src/main/java/dev/aransword/spring_ai_profile/SpringAiProfileApplication.java
@@ -0,0 +1,13 @@
+package dev.aransword.spring_ai_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aransword/spring_ai_profile/SpringAiProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aransword.spring_ai_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
